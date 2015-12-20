0c9edee6cc28717468ea44b354d6e392abc91aaa
Cristina Silva <cristina.silva@openmailbox.org>
2015-12-19 23:22:11 -0200

    Revert "Adiciona crawler de perfis de politicos"

--- 99Politicos/src/main/java/com/nnpoliticos/crawler/normalize/Normalizer.java
+++
@@ -1,6 +0,0 @@
-package com.nnpoliticos.crawler.normalize;
-
-public interface Normalizer extends Runnable {
-	
-	public void run();
-}
--- 99Politicos/src/main/java/com/nnpoliticos/crawler/robots/Robots.java
+++
@@ -1,6 +0,0 @@
-package com.nnpoliticos.crawler.robots;
-
-public interface Robots extends Runnable {
-
-	public void run();
-}
